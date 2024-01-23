6ee3d9b14300b537318cdb9b6f118b62d30d8276
akshay ashok <akshayashokan1054@gmail.com>
2024-01-23 20:50:10 +0530

    Initial Commit

---
+++ Animated-SwiftUI/Animated_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animated_SwiftUIApp.swift
+//  Animated-SwiftUI
+//
+//  Created by Akshay Ashok on 23/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Animated_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
